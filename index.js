40f177963acf1d78a0419750d9fc5b4889647a90
Lei Zhao <leizhao4@gmail.com>
2016-10-31 18:22:10 -0500

    Import webcam.swf into assets folder

--- index.js
+++ index.js
@@ -2,5 +2,16 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-webcam'
+  name: 'ember-webcam',
+  init() {
+    this._super.init && this._super.init.apply(this, arguments);
+    let assetsPath = require('path').join('webcamjs', 'webcam.js');
+    this.treePaths['vendor'] =
+      require.resolve('webcamjs').replace(assetsPath, '');
+  },
+  included() {
+    this.app.import('vendor/webcamjs/webcam.swf', {
+      destDir: 'assets'
+    });
+  }
 };
